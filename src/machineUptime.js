2309c6210b67f76e160ae5caf38e2d8deeccb349
Mark Liang <mark@vdr.tw>
2025-08-05 13:59:34 +0800

    feat: 增加故障機率

--- src/machineUptime.js
+++ src/machineUptime.js
@@ -66,11 +66,11 @@ export default function handleChart(request) {
 	return new Response(JSON.stringify(result), { status: 200, headers: handleCORSHeaders(request) });
 
 	function generateRandomTime() {
-		const randomStopCounts = Math.floor(Math.random() * 3);
+		const randomStopCounts = Math.floor(Math.random() * 10);
 
 		return Array.from({ length: randomStopCounts })
 			.map(() => {
-				const randomStopDurationGenerator = Math.floor(Math.random() * 20) + 10; // 停機時間 10 ~ 30 分鐘
+				const randomStopDurationGenerator = Math.floor(Math.random() * 60) + 30; // 停機時間 30 ~ 90 分鐘
 				const randomStopStart = Math.floor(Math.random() * 24 * 6) * 10; // 隨機停機開始時間，單位為10分鐘
 				const startTime = dayjs(date).startOf('day').add(randomStopStart, 'minute');
 				const endTime = startTime.add(randomStopDurationGenerator, 'minute');
